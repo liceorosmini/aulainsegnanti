2fb5028853249c1c8536bfb82617a524912068d2
tizianocescatti <tiziano.cescatti@liceorosmini.eu>
2024-03-20 16:53:13 +0100

    nuova aula insegnanti

---
+++ data.js
@@ -0,0 +1,365 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ingresso---area-studio",
+      "name": "Ingresso - Area studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14064704049563836,
+          "pitch": 0.07857288010221808,
+          "rotation": 4.71238898038469,
+          "target": "1-area-postazioni-informatiche"
+        },
+        {
+          "yaw": 0.11153864669447167,
+          "pitch": 0.07890785035872483,
+          "rotation": 1.5707963267948966,
+          "target": "2-area-riunioni"
+        },
+        {
+          "yaw": -0.01562825503181209,
+          "pitch": -0.03708989910983185,
+          "rotation": 0,
+          "target": "3-area-polivalente"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6572918856814169,
+          "pitch": 0.06741947158725559,
+          "title": "Title",
+          "text": "Armadietti docenti"
+        },
+        {
+          "yaw": 1.407534268638063,
+          "pitch": 0.36011177069880773,
+          "title": "Title",
+          "text": "postazioni di lavoro con alimentazioni per notebook"
+        },
+        {
+          "yaw": -1.3433144272582496,
+          "pitch": 0.3569529315967408,
+          "title": "Title",
+          "text": "postazioni di lavoro con alimentazione per notebook"
+        },
+        {
+          "yaw": -1.4013336437714656,
+          "pitch": 0.042698163818643664,
+          "title": "Title",
+          "text": "armadietti docenti"
+        },
+        {
+          "yaw": -0.584894341754957,
+          "pitch": 0.061198268928652766,
+          "title": "Title",
+          "text": "armadietti docenti"
+        },
+        {
+          "yaw": -2.1463942689241193,
+          "pitch": 0.08790554681761797,
+          "title": "Title",
+          "text": "armadietti docenti"
+        },
+        {
+          "yaw": -3.1076820674245145,
+          "pitch": 0.12875763002068297,
+          "title": "Title",
+          "text": "porta di uscita/ingresso"
+        },
+        {
+          "yaw": 2.2897737629209587,
+          "pitch": 0.10461193322061213,
+          "title": "Title",
+          "text": "armadietti docenti"
+        }
+      ]
+    },
+    {
+      "id": "1-area-postazioni-informatiche",
+      "name": "Area postazioni informatiche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.4532537445177454,
+        "pitch": 0.10512686961197915,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.028970348129245593,
+          "pitch": 0.10051729664805897,
+          "rotation": 0,
+          "target": "3-area-polivalente"
+        },
+        {
+          "yaw": 0.3225255843504744,
+          "pitch": 0.08432382847793107,
+          "rotation": 1.5707963267948966,
+          "target": "2-area-riunioni"
+        },
+        {
+          "yaw": 3.0499546183560726,
+          "pitch": 0.1820282818736665,
+          "rotation": 0,
+          "target": "0-ingresso---area-studio"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7503978690025725,
+          "pitch": 0.15424839349467234,
+          "title": "Title",
+          "text": "postazioni desktop"
+        },
+        {
+          "yaw": -1.681074662438938,
+          "pitch": 0.09271663041617018,
+          "title": "Title",
+          "text": "postazioni desktop"
+        },
+        {
+          "yaw": -2.3000577810456857,
+          "pitch": 0.1347030169357808,
+          "title": "Title",
+          "text": "armadietti docenti"
+        },
+        {
+          "yaw": -1.0008596355638701,
+          "pitch": 0.2702814806413585,
+          "title": "Title",
+          "text": "armadio tecnico"
+        },
+        {
+          "yaw": -1.0356381001202664,
+          "pitch": 0.09658219253710243,
+          "title": "Title",
+          "text": "stampante di rete"
+        }
+      ]
+    },
+    {
+      "id": "2-area-riunioni",
+      "name": "Area riunioni",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.6242181335057673,
+        "pitch": 0.10012082820187373,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1902105327829453,
+          "pitch": 0.12527826272293652,
+          "rotation": 17.27875959474387,
+          "target": "1-area-postazioni-informatiche"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8627586310872246,
+          "pitch": 0.6097816098224111,
+          "title": "Title",
+          "text": "punti di alimentazione notebook"
+        },
+        {
+          "yaw": 0.7987939300912856,
+          "pitch": 0.5707862261747856,
+          "title": "Title",
+          "text": "punti di alimentazione notebook"
+        },
+        {
+          "yaw": -1.49544124033363,
+          "pitch": 0.005075585695077933,
+          "title": "Title",
+          "text": "punto alimentazione maxischermo"
+        }
+      ]
+    },
+    {
+      "id": "3-area-polivalente",
+      "name": "Area polivalente",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.8156762343078565,
+        "pitch": 0.21276440327420687,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1233609766651416,
+          "pitch": 0.08438207889773963,
+          "rotation": 0,
+          "target": "0-ingresso---area-studio"
+        },
+        {
+          "yaw": -2.898348250380897,
+          "pitch": 0.1299518526672454,
+          "rotation": 1.5707963267948966,
+          "target": "1-area-postazioni-informatiche"
+        },
+        {
+          "yaw": 2.9812091054602874,
+          "pitch": 0.1634447941684325,
+          "rotation": 4.71238898038469,
+          "target": "2-area-riunioni"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1385253362919094,
+          "pitch": 0.2249659494501568,
+          "title": "Title",
+          "text": "lavello"
+        },
+        {
+          "yaw": -2.3186114614317344,
+          "pitch": 0.2660463022278048,
+          "title": "Title",
+          "text": "forno a microonde"
+        },
+        {
+          "yaw": -1.997353269018614,
+          "pitch": 0.1760715005221023,
+          "title": "Title",
+          "text": "frigorifero/congelatore"
+        },
+        {
+          "yaw": 1.7598650823347874,
+          "pitch": 0.4196833956567101,
+          "title": "Title",
+          "text": "spazio relax/lettura"
+        },
+        {
+          "yaw": -1.0511864999730651,
+          "pitch": 0.39584974056079325,
+          "title": "Title",
+          "text": "tavoli ad uso ricreativo o per lavoro"
+        },
+        {
+          "yaw": -0.08729224829185966,
+          "pitch": 0.35905175796190925,
+          "title": "Title",
+          "text": "armadietti contenitori multiuso condivisi"
+        },
+        {
+          "yaw": -1.9118666638288975,
+          "pitch": -0.2969804665918865,
+          "title": "Title",
+          "text": "access point"
+        },
+        {
+          "yaw": 0.01976898398534388,
+          "pitch": -0.4655913054060097,
+          "title": "Title",
+          "text": "proiettore"
+        },
+        {
+          "yaw": -1.0255889437099164,
+          "pitch": -0.2545812655066033,
+          "title": "Title",
+          "text": "cassa acustica"
+        },
+        {
+          "yaw": 0.9666996858579342,
+          "pitch": -0.2593002529191004,
+          "title": "Title",
+          "text": "cassa acustica"
+        },
+        {
+          "yaw": 0.584927908773679,
+          "pitch": 0.06270349900316319,
+          "title": "Title",
+          "text": "uscita di emergenza"
+        }
+      ]
+    }
+  ],
+  "name": "Nuova aula insegnanti Liceo A. Rosmini",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
